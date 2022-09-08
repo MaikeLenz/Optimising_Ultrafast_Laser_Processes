73b28d36189b357084ab83e8f4280140ac4080be
iammollys <iammollysmith@gmail.com>
2022-09-08 15:18:51 +0100

    added comments to the example code

--- Examples/example_optimisation.py
+++ Examples/example_optimisation.py
@@ -1,43 +1,47 @@
 import sys
 import csv
 sys.path.append('C:\\Users\\iammo\\Documents\\Optimising_Ultrafast_Laser_Processes\\Optimisation_Functions\\')
-from OptimisationFunction import *
+from optimisation_function import *
 
+# First choose the paramters you wish to vary
+# Values:  radius, flength, gas, pressure, wavelength, energy, τfwhm, grating_pair_separation
 params=["energy", "pressure", "grating_pair_displacement", "radius", "flength"]
 
-#values:  radius, flength, gas, pressure, wavelength, energy, τfwhm, grating_pair_separation
-gas="He"
-# Define experimental params
-
+# Define experimental parameters
+# Initial values must be chosen even for the parameters you are optimising so that an initial simulation can be run
+gas="He" # The gas to fill the HCF with
 radius = 100e-6 # HCF core radius
 flength = 3 # HCF length
-wavel=800e-9
-duration=30e-15
-energy_init=0.75e-3
-pressure_init=0.66*3
-grating_init=0
+wavel = 800e-9 # Central wavelength of the input laser pulse
+duration = 30e-15 # Initial pulse duration
+energy_init = 0.75e-3 # Energy of the input pulse
+pressure_init = 0.66*3 # Pressure within the HCF
+grating_init = 0 # Compressor grating separation
 
-initial_values_HCF=[radius, flength, gas,pressure_init, wavel, energy_init,duration, grating_init]
+initial_values_HCF = [radius, flength, gas, pressure_init, wavel, energy_init, duration, grating_init] # These values must remain in the order given here to be passed into the optimisation function
 
+# Choose the number of initial points and number of iterations for Bayesian optimisation to run for
+# We would recommend starting with one initial point and one iteration to test the optimisation function
+inits = 1
+iters = 1
 
-inits=1
-iters=1
-result,iterations=Luna_BO(params, initial_values_HCF, function=max_peak_power_1300nm_quadratic_phase, init_points=inits, n_iter=iters)
+# Run the optimisation function
+result, iterations = Luna_BO(params, initial_values_HCF, function=max_peak_power_1300nm_quadratic_phase, init_points=inits, n_iter=iters)
+
+# Extract results
 target = result['target']
-grating=result["params"]["grating_pair_displacement"]
-pressure=result["params"]["pressure"]
-energy=result["params"]["energy"]
-radius=result["params"]["radius"]
-flength=result["params"]["flength"]
+grating = result["params"]["grating_pair_displacement"]
+pressure = result["params"]["pressure"]
+energy = result["params"]["energy"]
+radius = result["params"]["radius"]
+flength = result["params"]["flength"]
 
-"""
+# Below is a suggestion for how the results can be saved in a csv file
+savefile_name = 'C:\\Users\\iammo\\Documents\\Optimising_Ultrafast_Laser_Processes\\example_optimisation_results.csv'
 header = ['init_points', 'n_iter', 'peak power', 'energy, J', 'pressure, bar', 'radius, m', 'flength, m', 'FWHM, s', 'wavel, m', 'gas', 'grating_pair_displacement, m']
-#with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_Luna\\data\\peak_power_1200e-9wavelwindow_varyfwhm__init_' + str(init_points) + '_niter_' + str(n_iter) + '.csv', 'a', encoding='UTF8', newline='') as f:
-with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_with_FT_and_phase\\data\\1300nm_phasecondition\\1300nm_He__init_50_niter_1000.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f) # peak_power_1000e-9wavelwindow__init_50_niter_100
+with open(savefile_name, 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
     # write the header
     writer.writerow(header)
-
     # write the data
     writer.writerow([inits, iters, target, energy, pressure, radius, flength, duration, wavel, gas, grating])
-"""
